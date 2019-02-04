65376420e342bfbc681962f862eb5d6e0d9c629e
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-02-04 18:38:54 -0500

    Install specs, write first two tests

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User visits the homepage' do
+  scenario 'successfully' do
+    visit root_path 
+
+    expect(page).to have_css 'h1', text: 'Setlister'
+  end
+end
+
+feature 'The homepage allows the user to enter song title' do
+  scenario 'successfully' do
+    visit root_path
+    fill_in('search', with: 'Good Riddance')
+    click_on 'Add song'
+
+    expect(page).to have_css 'li', text: 'Good Riddance'
+  end
+end
